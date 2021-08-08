f006e14f9fe225580ce5883fab4fa5c71ede16de
Vinicius Alves <viniciusalves@MacBook-Pro-de-Vinicius.local>
2021-08-08 19:44:05 -0300

    Group Routers and fix htaccess

--- routers/web.php
+++ routers/web.php
@@ -11,11 +11,15 @@ SimpleRouter::get('/list/all/user/{user}/by/state/{state}', [AppController::clas
 SimpleRouter::post('/auth/sign/in', [AuthController::class, 'signIn'])->name('auth.sigin');
 SimpleRouter::get('/auth/sign/out', [AuthController::class, 'signOut'])->name('auth.signout');
 
-SimpleRouter::get('/ticket/state/{state}', [TicketController::class, 'show'])->name('ticket.all.state');
-SimpleRouter::get('/ticket/by/id/{id}', [TicketController::class, 'show'])->name('ticket.show');
-SimpleRouter::get('/ticket/create/user/{user}', [TicketController::class, 'viewStore'])->name('ticket.store.view');
-SimpleRouter::form('/ticket/create/user/{user}/new', [TicketController::class, 'store'])->name('ticket.store');
+SimpleRouter::group(['prefix' => 'ticket'], function() {
+    SimpleRouter::get('/state/{state}', [TicketController::class, 'show'])->name('ticket.all.state');
+    SimpleRouter::get('/by/id/{id}', [TicketController::class, 'show'])->name('ticket.show');
+    SimpleRouter::get('/create/user/{user}', [TicketController::class, 'viewStore'])->name('ticket.store.view');
+    SimpleRouter::form('/create/user/{user}/new', [TicketController::class, 'store'])->name('ticket.store');
+});
 
-SimpleRouter::post('/request/type/category', [HttpController::class, 'category'])->name('request.category');
-SimpleRouter::post('/request/type/fields', [HttpController::class,'fields'])->name('request.category');
-SimpleRouter::post('/request/type/entity', [HttpController::class,'entity'])->name('request.entity');
+SimpleRouter::group(['prefix' => 'request'], function () {
+    SimpleRouter::post('/type/category', [HttpController::class, 'category'])->name('request.category');
+    SimpleRouter::post('/type/fields', [HttpController::class,'fields'])->name('request.category');
+    SimpleRouter::post('/type/entity', [HttpController::class,'entity'])->name('request.entity');
+});
