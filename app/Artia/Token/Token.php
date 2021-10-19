14ef811920c787fcaacc5829db3715cfb1d5b860
Vinicius Coutinho <vinicius@promofarma.com.br>
2021-10-19 12:46:57 -0300

    :bug: Correção de atualização do token

--- app/Artia/Token/Token.php
+++ app/Artia/Token/Token.php
@@ -10,13 +10,13 @@ class Token
 {
     /**
      * @var string
-    */
+     */
     private static $hash;
 
     /**
      * @return void
-    */
-    public static function regenerate(): void
+     */
+    public static function regenerate()
     {
         if (Token::loadCacheFile()) {
             $logger = new Logger('token');
@@ -28,34 +28,36 @@ class Token
             $last = date_create($file->date);
             $now->modify('-40 minutes');
 
-            if ($now > $last) {
-                $api = new Api();
-                $api->requireds([
-                        'clientId' => env('CONFIG_API_CLIENT_ID'),
-                        'secret' => env('CONFIG_API_SECRET')
-                ])->authenticationByClient();
-
-                $response = $api->response();
-
-                if (isset($response->errors[0])) {
-                    $logger->warning($response->errors[0]->message);
-                }
-
-                $fopen = fopen(__DIR__ . env('CONFIG_API_TOKEN_CACHE'), 'w+');
-                fwrite($fopen, json_encode([
-                    'cache' => 'token',
-                    'token' => $response->data->authenticationByClient->token,
-                    'type'  => 'authenticationByClient',
-                    'date'  => date('Y-m-d H:i:s')
-                ]));
-                fclose($fopen);
+            if ($now < $last) {
+                return null;
             }
+
+            $api = new Api();
+            $api->requireds([
+                'clientId' => env('CONFIG_API_CLIENT_ID'),
+                'secret' => env('CONFIG_API_SECRET')
+            ])->authenticationByClient();
+
+            $response = $api->response();
+
+            if (isset($response->errors[0])) {
+                $logger->warning($response->errors[0]->message);
+            }
+
+            $fopen = fopen(__DIR__ . env('CONFIG_API_TOKEN_CACHE'), 'w+');
+            fwrite($fopen, json_encode([
+                'cache' => 'token',
+                'token' => $response->data->authenticationByClient->token,
+                'type'  => 'authenticationByClient',
+                'date'  => date('Y-m-d H:i:s')
+            ]));
+            fclose($fopen);
         }
     }
 
     /**
      * @return null|string
-    */
+     */
     public static function hash(): ?string
     {
         return self::$hash;
@@ -63,13 +65,14 @@ class Token
 
     /**
      * @return object
-    */
+     */
     public static function loadCacheFile(): object
     {
         $file = __DIR__ . env('CONFIG_API_TOKEN_CACHE');
 
         if (!file_exists($file)) {
-            return new class {
+            return new class
+            {
                 public $date = '2021-01-01 00:00:00';
             };
         }
